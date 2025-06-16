b1d7054f1501c1fc6a9aba6f0e3935eec0b67230
Rohan23 <rohan.359.2024@doonschool.com>
2025-06-16 22:50:53 +0530

    fixed typo.

--- src/processing/test_sentiment.py
+++ src/processing/test_sentiment.py
@@ -1,4 +1,4 @@
-from src.processing.sentiment_analysis import vader_statement
+from src.processing.sentiment_analysis import vader_sentiment
 
 sample_text = "This stock is doing great!"
 print(vader_sentiment(sample_text))
